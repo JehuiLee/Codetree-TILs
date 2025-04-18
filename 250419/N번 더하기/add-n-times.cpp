bc22f0b974c7d0e09de49a1c3a12880dc89165b4
Lee Jehui <142780364+JehuiLee@users.noreply.github.com>
2025-04-19 01:23:30 +0900

    Title: N번 더하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250419/N번 더하기/add-n-times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, N, sum;
+    cin >> A >> N;
+    sum = A + N;
+    for (int i = 0; i < N; i++) {
+        cout << sum << endl;
+        sum += N;
+    }
+    return 0;
+}
