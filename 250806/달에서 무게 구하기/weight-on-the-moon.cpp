e8c5ae792794a13eb6458d7fa0a11b8ff15026f8
Lee Jehui <142780364+JehuiLee@users.noreply.github.com>
2025-08-06 01:45:10 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250806/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+
+    int a = 13;
+    double b = 0.165000;
+
+    cout.precision(6);
+
+    cout << a << " * " << b << " = " << a * b;
+
+    return 0;
+}
