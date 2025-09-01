7659c0cdc1e1705666c5ae87fb1f746e6a4c1409
Lee Jehui <142780364+JehuiLee@users.noreply.github.com>
2025-09-01 14:28:57 +0900

    Title: 배수의 개수 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250901/배수의 개수 2/number-of-multipliers-2.cpp
+++ 250901/배수의 개수 2/number-of-multipliers-2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int a, cnt = 0;
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 10; i++) {
         cin >> a;
         if (a % 2 == 1) cnt++;
     }
